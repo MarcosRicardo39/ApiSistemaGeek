a84444caaadcc9a985e2c41d24d6d9bad6cc9618
vinigavea39-gif <vinigavea39@gmail.com>
2026-05-09 19:28:10 -0300

    Mudança de nome do name space

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -1,18 +1,25 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiSistemaGeek.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiSistemaGeek.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProdutosController : ControllerBase
+    public class ProdutoGeekController : ControllerBase
     {
+        private readonly AppDbContext _context;
 
+        public ProdutoGeekController(AppDbContext context)
+        {
+            _context = context;
+        }
 
-        
-
-
-
-
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var dados = await _context.ProdutosGeek.ToListAsync();
+            return Ok(dados);
+        }
     }
 }
