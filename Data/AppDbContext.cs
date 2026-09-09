2427400faf236dd7f089e0298b53e02edc81da55
MarcosRicardo39 <vinigavea39@outlook.com>
2026-09-09 14:20:18 -0300

    Inclusão da classe estoque no banco

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,26 +1,35 @@
-﻿    using ApiSistemaGeek.Model;
-    using Microsoft.EntityFrameworkCore;
-    using ApiSistemaGeek.Data;
+﻿using ApiSistemaGeek.Model;
+using Microsoft.EntityFrameworkCore;
+using ApiSistemaGeek.Data;
 
 
 namespace ApiSistemaGeek.Data
 {
-        public class AppDbContext : DbContext
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
         {
-            public AppDbContext(DbContextOptions<AppDbContext> options)
-                : base(options)
-            {
-            }
+        }
 
-            public DbSet<ProdutoGeek> ProdutoGeek { get; set; }
-            public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<ProdutoGeek> ProdutoGeek { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
 
-            public DbSet<Pedido> Pedidos { get; set; }
-            public DbSet<PedidoItem> PedidoItens { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoItem> PedidoItens { get; set; }
+        public DbSet<Estoque> Estoques { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<ProdutoGeek>()
+                .Property(p => p.Preco)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Pedido>()
+                .Property(p => p.Total)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<PedidoItem>()
                 .Property(p => p.Preco)
                 .HasPrecision(18, 2);
 
@@ -33,8 +42,12 @@ namespace ApiSistemaGeek.Data
                 .HasOne<ProdutoGeek>()
                 .WithMany()
                 .HasForeignKey(p => p.ProdutoGeekId);
-        }
 
+            modelBuilder.Entity<Estoque>()
+                .HasOne<ProdutoGeek>()
+                .WithOne()
+                .HasForeignKey<Estoque>(e => e.ProdutoGeekId);
+        }
     }
 
 
