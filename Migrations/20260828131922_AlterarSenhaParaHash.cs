cd20e5927cb85d1280ead55c24ce29948c298f2c
MarcosRicardo39 <vinigavea39@outlook.com>
2026-09-09 11:00:25 -0300

    Modificações na loja

---
+++ Migrations/20260828131922_AlterarSenhaParaHash.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiSistemaGeek.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterarSenhaParaHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Senha",
+                table: "Usuarios",
+                newName: "SenhaHash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SenhaHash",
+                table: "Usuarios",
+                newName: "Senha");
+        }
+    }
+}
